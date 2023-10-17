71630861cef369fdce9860393cf361218e0b3419
alex <alex.musyoki@student.moringaschool.com>
2023-10-17 17:09:42 +0300

    Completed assignments

---
+++ index.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body')
+body.remove(main)
+
+//Add newHeader with ID victory
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+//Adding contents inside
+newHeader.innerHTML = "ALEX"
